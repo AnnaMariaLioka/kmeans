216cbb447891ecf8fb60654720f60d041e61b9ff
AnnaMariaLioka <lioka.annamaria@gmail.com>
2025-10-31 17:21:10 +0100

    Added code description in k_means_cluster.py

--- k_means_cluster.py
+++ k_means_cluster.py
@@ -10,45 +10,56 @@ Methodology for the K Means algorithm:
     Take mean of each class (mean of all featuresets by class), making that mean the new centroid
     Repeat steps 3-5 until optimized (centroids no longer moving)
 """
+#defaultdict = Dictionary, das automatisch leere Listen erzeugt (praktisch für Cluster-Listen)
+#typing = nur für saubere Typ-Hints im Code
 from collections import defaultdict
 from typing import Any, Dict, List, Tuple, Union
 
+'''
+matplotlib: zum Plotten der Cluster
+numpy: Mathe & Arrays
+cityblock: Manhattan-Distanz‐Funktion
+SAMPLE_DATA: Beispiel-Dataset aus Repo
+'''
 import matplotlib.pyplot as plt
 import numpy as np
 from pandas import DataFrame
 from scipy.spatial.distance import cityblock
-
 from make_clusters import SAMPLE_DATA
 
+#Alias für Datentypen, die als Input erlaubt sind:
 ClusterData = Union[List, np.array, np.ndarray, DataFrame]
 
-
+#Klasse, die k-means implementiert:
 class KMeansCluster:
     """Simple K-Means Clustering Model"""
-
+    
     def __init__(self, k: int = 2, tol: float = 0.001, max_iter: int = 300,
                  method: str = 'euclidean'):
+        #Sicherstellen, dass Methode gültig ist:
         if method not in {'euclidean', 'manhattan'}:
             raise ValueError('Method must be one of "euclidean" or "manhattan"')
-        self.k = k
-        self.tol = tol
-        self.max_iter = max_iter
-        self.method = method
+        #Speichert Parameter in Objekt:
+        self.k = k                                                                      #k = Anzahl Cluster
+        self.tol = tol                                                                  #tol: Toleranz für Konvergenz (wie stark dürfen Centroids sich noch bewegen)
+        self.max_iter = max_iter                                                        #max_iter: Abbruch nach X Iterationen
+        self.method = method                                                            #method: Distanzmethode (euclidean oder manhattan)
 
-        self.centroids: Dict[int, Any] = {}
-        self.clusters: Dict[int, Any] = defaultdict(list)
-        self.cluster_distances: Dict[int, Any] = {}
+        self.centroids: Dict[int, Any] = {}                                             #centroids: speichert die aktuellen Cluster-Zentren {cluster_id: point}
+        self.clusters: Dict[int, Any] = defaultdict(list)                               #clusters: speichert zugewiesene Punkte {cluster_id: [points]} und defaultdict(list) = automatisch leere Liste
+        self.cluster_distances: Dict[int, Any] = {}                                     #cluster_distances: wird später für Cluster-Qualität genutzt
 
+    #Zufällige Start-Zentren
     def initialize_centroids(self, data: ClusterData) -> "KMeansCluster":
         """Get initial centroids"""
-        choices = np.random.choice([*range(len(data))], size=self.k, replace=False)
+        choices = np.random.choice([*range(len(data))], size=self.k, replace=False)     #np.random.choice → wählt zufällig k Indexe
         for i in range(self.k):
             self.centroids[i] = data[choices[i]]
         return self
 
     @staticmethod
     def _convert_to_array(data: ClusterData) -> np.array:
-        """Function to convert data into ndarray"""
+        """Function to convert data into ndarray"""                                     #Konvertiert Input in NumPy-Array, egal ob Liste/DF
         if isinstance(data, np.ndarray):
             return data
         if isinstance(data, (List, Tuple)):
@@ -57,10 +68,12 @@ class KMeansCluster:
             return DataFrame.values
         raise TypeError
 
+    #Distanzberechnung:
     def get_distance(self, arr_1: ClusterData, arr_2: ClusterData) -> float:
         """Calculate squared distance between two points"""
         arr_1 = self._convert_to_array(arr_1)
         arr_2 = self._convert_to_array(arr_2)
+        #Konvertiert zu Arrays:
         if self.method == 'manhattan':
             dist = cityblock(arr_1, arr_2)
         else:
@@ -70,31 +83,38 @@ class KMeansCluster:
     @staticmethod
     def find_min_idx(distances: List) -> int:
         """Find the index of the minimum distance"""
-        return distances.index(min(distances))
+        return distances.index(min(distances))                                          #Gibt Index des nächstgelegenen Zentroids zurück
 
+    #Zuweisung: Welcher Punkt in welchen Cluster?
     def assign_cluster(self, data: ClusterData) -> "KMeansCluster":
         """Function to assign a point to a centroid"""
+        #Reset aller Cluster:
         data = self._convert_to_array(data)
         self.clusters.clear()
+        #für jeden Punkt → Distanz zu jedem Centroid berechnen
         for point in data:
             distance = []
             for centroid in self.centroids.values():
                 distance.append(self.get_distance(point, centroid))
+            #finde kleinste Distanz → Punkt zu diesem Cluster
             cluster = self.find_min_idx(distance)
             self.clusters[cluster].append(point)
         return self
 
+    #Centroid Update
     def update_centroid(self) -> "KMeansCluster":
         """Function to update centroids"""
         for cluster in self.clusters:
-            new_centroid = np.mean(np.array(self.clusters[cluster]), axis=0)
+            new_centroid = np.mean(np.array(self.clusters[cluster]), axis=0)            #Mittelwert aller Punkte im Cluster = neuer Zentroid  ==>Standard-K-Means Kernidee!
             self.centroids[cluster] = new_centroid
         return self
 
+    #Prüfen, ob Centroid-Bewegung klein genug:
     def meets_tolerance(self, old_centroids: ClusterData, new_centroids: ClusterData) -> bool:
         """Function to detect convergence"""
         return np.abs(self.get_distance(old_centroids, new_centroids)) <= self.tol
 
+    #Trainingsloop:
     def fit(self, data: ClusterData, verbose=0):
         """
         Function to fit K-Means object to dataset.
@@ -106,19 +126,22 @@ class KMeansCluster:
         if verbose not in {0, 1, 2}:
             raise ValueError('Verbose must be set to {0, 1, 2}')
         data = self._convert_to_array(data)
-        self.initialize_centroids(data).assign_cluster(data)
+        self.initialize_centroids(data).assign_cluster(data)                           #Start-Zentren und Erste Zuordnung
         if verbose > 0:
             print(f'Initial clusters: {self.clusters.items()}')
         converged = {}
         i = 0
+        #jede Iteration neu zuordnen:
         while i <= self.max_iter:
             self.assign_cluster(data)
+            #Wichtig: Für jede Cluster-Mitte prüfen: bewegt sie sich noch?
             for centroid in self.centroids:
                 old_centroid = self.centroids[centroid]
                 new_centroid = self.update_centroid().centroids[centroid]
                 converged[centroid] = self.meets_tolerance(old_centroid, new_centroid)
             if verbose > 1:
                 print(f'Iteration number: {i+1}')
+            #Wenn alle Centroids stabil → fertig.
             if all(converged.values()):
                 break
             i += 1
@@ -127,6 +150,7 @@ class KMeansCluster:
             print(f'Converged in {i+1} iterations.')
         return self
 
+    #Plot Evaluation - optional; Nicht algorithmisch wichtig, nur fürs Anzeigen.
     def plot(self) -> None:
         """Plot clusters and centroids"""
         for cluster in self.clusters.values():
